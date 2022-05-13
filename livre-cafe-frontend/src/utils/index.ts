2a2c7a40018042022a83263b5aba2d7a54bc967e
Luat <you@example.com>
2022-05-13 19:02:10 +0700

    Mock Data Books, create Books Table

--- livre-cafe-frontend/src/utils/index.ts
+++ livre-cafe-frontend/src/utils/index.ts
@@ -1,5 +1,14 @@
-import { customError } from "@app/redux-toolkit/features/api/api.interface";
-
-export const getError = (error: customError) => {
-  return (error.data && error.data.message) || error.message;
-};
+export function stableSort<T>(
+  array: readonly T[],
+  comparator: (a: T, b: T) => number,
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
