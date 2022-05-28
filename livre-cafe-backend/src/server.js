ea42afb822537a6813ed0395db576a4203cb7ce8
Haibeo <h0000151515@gmail.com>
2022-05-28 10:27:29 +0700

    complete books and drinks api

--- livre-cafe-backend/src/server.js
+++ livre-cafe-backend/src/server.js
@@ -6,14 +6,14 @@ const app = require('./app');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
-const mongoUri = 'mongodb://127.0.0.1:27017/MyDB';
+const mongoUri = 'mongodb+srv://Halley:0913127854Hai@mydb.0us8q.mongodb.net/LivreCoffeeDB?retryWrites=true&w=majority';
 
 async function startServer() {
     try {
         await mongoose.connect(mongoUri);
         server.listen(PORT, () => {
             console.log("Server is listening...");
-        })
+        });
     } catch (err) {
         console.log(err);
         console.log("Database connection failed!");
