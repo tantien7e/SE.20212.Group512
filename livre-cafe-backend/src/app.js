ca6c6104f184c90dd1b1e5b9a1ea1965dd5e49db
Haibeo <h0000151515@gmail.com>
2022-06-29 22:07:33 +0700

    Add token verification route

--- livre-cafe-backend/src/app.js
+++ livre-cafe-backend/src/app.js
@@ -9,6 +9,7 @@ const DrinksRouter = require('./api/drinks/drinks.route');
 const OrdersRouter = require('./api/orders/orders.route');
 const CustomerRouter = require('./api/customers/customers.route');
 const StaffsRouter = require('./api/staffs/staffs.route');
+const JwtVerification = require('./api/jwt/jwtVerification.route');
 
 const app = express();
 
@@ -43,6 +44,7 @@ app.use(cors({
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
+app.use('/token', JwtVerification);
 app.use('/staffs', StaffsRouter);
 app.use(passport.authenticate('jwt', {session: false}));
 app.use('/books', BooksRouter);
