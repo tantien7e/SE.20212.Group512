f87bb9ed26f9a04fcc0b6c0ab2e0061c1bc58963
Luat <you@example.com>
2022-07-07 13:12:01 +0700

    .

--- livre-cafe-frontend/src/assets/images/index.ts
+++ livre-cafe-frontend/src/assets/images/index.ts
@@ -2,13 +2,11 @@ export { ReactComponent as Logo } from './logo.svg';
 import femalePic from './female-gender.svg';
 import malePic from './male-gender.svg';
 import naPic from './na-gender.svg';
-import huyen from './huyennytoi.svg';
 
 const IMAGES = {
   femalePic,
   malePic,
   naPic,
-  huyen,
 };
 
 export default IMAGES;
